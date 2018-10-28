2ded2c868d2e2754e90f4396117df36ff51064ea
zhaoliang <18731965636@163.com>
2018-10-28 21:46:53 +0800

    styl变量应用 webpack配置修改 iconfont引用

---
+++ travel/src/pages/home/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe600;</div>
+    </div>
+    <div class="header-input">
+        <span class="iconfont">&#xe86e;</span>
+        输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+        城市
+        <span class="iconfont arrow-icon">&#xe601;</span>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+
+</script >
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+    .header
+        display:flex
+        line-height:.86rem
+        background:$bgcolor
+        color:#fff
+    .header-left
+        width:.64rem
+        float:left
+      .back-icon
+        text-align:center
+        font-size:.4rem
+    .header-input
+        flex:1
+        margin-top:.12rem
+        margin-left:.2rem
+        padding-left:.2rem
+        height:.64rem
+        line-height:.64rem
+        background:#fff
+        border-radius:.1rem
+        color:#ccc
+    .header-right
+        width:1.24rem
+        float:right
+        text-align:center
+      .arrow-icon
+            marign-left:-0.4rem
+            font-size:.24rem
+</style>
