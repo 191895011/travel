27a544a959f66a4d12f118b4f99fe4e8afb146a5
zhaoliang <18731965636@163.com>
2018-10-31 20:49:51 +0800

    swiper插件

---
+++ travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="wrapper">
+   <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+          <img  class="swiper-img" :src="item.imgUrl" alt="" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+</div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1708/f1/ee65a13642c95c02.jpg_750x200_3440fec0.jpg'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/ff/1bf7d3a4f6591002.jpg_750x200_40809f2a.jpg'
+      }, {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/d4/8c74e56397d8f002.jpg_750x200_81d8c2c5.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style type="text/css" lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+    background:#fff
+.wrapper
+  overflow:hidden
+  width:100%
+  height:0
+  padding-bottom:27%
+  background:#eee
+  .swiper-img
+     width:100%
+</style>
